ecdafc6678d9f3ee8d82630e4e445ce180bef203
EC2 Default User <ec2-user@ip-172-31-20-153.ec2.internal>
2022-02-14 09:15:49 +0000

    [Fix]カラム名、データ型

--- db/migrate/20220213104559_create_items.rb
+++ db/migrate/20220213104559_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :explanation
-      t.integer :tax_out_price
-      t.string :image
+      t.string :name, null: false
+      t.text :explanation, null: false
+      t.integer :tax_out_price, null: false
+      t.string :image_id, null: false
       t.timestamps
     end
   end
