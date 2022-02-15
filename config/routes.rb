956fb067d4e18f1a9be7d2c92141b48d0df4790a
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-02-15 05:08:52 +0000

    コントローラー・ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,40 @@
 Rails.application.routes.draw do
+  namespace :customer do
+    get 'orders/new'
+    get 'orders/log'
+    get 'orders/index'
+    get 'orders/thanx'
+    get 'orders/show'
+  end
+  namespace :customer do
+    get 'cart_items/index'
+  end
+  namespace :customer do
+    get 'items/top'
+    get 'items/about'
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :customer do
+    get 'customers/edit'
+    get 'customers/show'
+  end
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/edit'
+    get 'items/show'
+    get 'items/top'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/edit'
+    get 'customers/show'
+  end
   devise_for :admin, skip: [:registrations, :passwords],controllers: {
     sessions: "admin/sessions"
   }
