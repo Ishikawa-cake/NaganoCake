fa1eb3614852cfcaaca64183e19e89c334813121
EC2 Default User <ec2-user@ip-172-31-38-68.ec2.internal>
2022-02-16 07:32:44 +0000

    M

--- app/controllers/customer/registrations_controller.rb
+++ app/controllers/customer/registrations_controller.rb
@@ -62,11 +62,11 @@ class Customer::RegistrationsController < Devise::RegistrationsController
   # end
   
   def after_sign_in_path_for(resource)
-    customer_path(resource)
+    about_path(resource)
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :kana_family_name,:kana_first_name, :postal_code, :address, :tel, ])
   end
   
 end
