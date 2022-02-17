0344d0047d7d3b8a4487b82fdb4b5c89e1f6f55c
shinnkai <fairytear911@gmail.com>
2022-02-17 08:52:12 +0000

    viewのitems作成

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -5,9 +5,16 @@ class Customer::ItemsController < ApplicationController
   def about
   end
 
+  
   def index
+    @items = Item.all
+    
   end
 
   def show
+    @item = Item.find(params[:id])
+    #カートに追加するため
+    @cart_items = CartItem.new
   end
+  
 end
