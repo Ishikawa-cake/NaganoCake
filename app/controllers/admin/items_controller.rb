12e92c33395ce46fa2ed135f7006787ab20bc772
EC2 Default User <ec2-user@ip-172-31-38-68.ec2.internal>
2022-02-18 14:50:11 +0000

    registration view customer fix

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -47,7 +47,7 @@ class Admin::ItemsController < ApplicationController
 private
 
   def item_params
-    params.require(:item).permit(:name, :explanation, :tax_out_price, :image)
+    params.require(:item).permit(:image, :name, :explanation, :tax_out_price)
   end
 
 end
