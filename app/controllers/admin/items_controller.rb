f0848ff0279738092dd439222b7c7f457461cf25
EC2 Default User <ec2-user@ip-172-31-20-153.ec2.internal>
2022-02-19 06:09:43 +0000

    fix

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,7 +21,7 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    
+
     if @item.save
       redirect_to admin_items_path(@item)
     else
