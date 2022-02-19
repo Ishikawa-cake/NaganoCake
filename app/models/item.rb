104b3944352c39bb0180c64a89037770f2eb00f0
EC2 Default User <ec2-user@ip-172-31-38-68.ec2.internal>
2022-02-19 04:57:00 +0000

    M

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,10 @@ class Item < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :cart_items, dependent: :destroy
 
+  
+  
   has_one_attached :image
-  def get_profile_image(size)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'no_.jpg', content_type: 'image/jpeg')
-  end
-  profile_image.variant(resize: size).processed
-  end
+  
+  
 
 end
