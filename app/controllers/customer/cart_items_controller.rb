81cb6731f4d1264d9245ed747b474239dc0bd9d7
EC2 Default User <ec2-user@ip-172-31-20-153.ec2.internal>
2022-02-20 11:11:22 +0000

    レイアウト調整

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -6,12 +6,23 @@ class Customer::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.save
-    redirect_to customer_cart_items_path
-
+    @cart_item.customer_id = current_customer.id
+    if my_cart.find_by(item_id: params[:cart_item][:item_id]).present?
+			item_in_cart = my_cart.find_by(item_id: params[:cart_item][:item_id])
+			item_in_cart.quantity += params[:cart_item][:quantity].to_i
+			item_in_cart.save
+      flash[:notice] = '商品を追加でカートに入れました。'
+      redirect_to customer_cart_items_path
+    elsif @cart_item.save
+      flash[:notice] = '商品をカートに入れました。'
+      redirect_to customer_cart_items_path
+    else
+      flash[:alert] = '商品の追加に失敗しました。'
+      redirect_to item_path(params[:item_id])
+    end
   end
 
-  
+
   private
 
   def my_cart
