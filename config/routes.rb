4508f714344d42760a57e89605e1a357021fd0d2
EC2 Default User <ec2-user@ip-172-31-38-68.ec2.internal>
2022-02-22 07:11:21 +0000

    routes.rb fix

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
      end
 
      resources :order_items
-     
+
     end
     resources :items, only: [:index, :show]
     resources :cart_items, only: [:index, :create, :destroy, :update]
